86810605d4e67d065b5f5c97dd5904ab00f49243
aitacara <aitacara@tcd.id>
2017-10-19 23:12:20 +0100

    Added more tests for deeper in tree structure

--- Test.py
+++ Test.py
@@ -8,13 +8,25 @@ class TestLCAMethod(unittest.TestCase):
         self.assertEqual(LCA.lowestCommonAncestor(dummy.root ,dummy.search(5), dummy.search(1)),dummy.search(3))
 
     def test_Root_As_Result(self):
-        self.assertEqual(LCA.lowestCommonAncestor(0 ,0, 0),True)
+        self.assertEqual(LCA.lowestCommonAncestor(dummy.root ,dummy.search(6), dummy.search(8)),dummy.search(3))
 
     def test_Both_Desendants_Same(self):
-        self.assertEqual(LCA.lowestCommonAncestor(0 ,0, 0),True)
+        self.assertEqual(LCA.lowestCommonAncestor(dummy.root ,dummy.search(8), dummy.search(8)),dummy.search(1))
 
     def test_Both_Desendants_Same_As_Root(self):
-        self.assertEqual(LCA.lowestCommonAncestor(0 ,0, 0),True)
+        self.assertEqual(LCA.lowestCommonAncestor(dummy.root ,dummy.search(3), dummy.search(3)),dummy.search(3))
+
+    def test_Mid_Tree_Result_Right(self):
+        self.assertEqual(LCA.lowestCommonAncestor(dummy.root ,dummy.search(0), dummy.search(8)),dummy.search(1))
+
+    def test_Mid_Tree_Result_Left(self):
+        self.assertEqual(LCA.lowestCommonAncestor(dummy.root ,dummy.search(2), dummy.search(6)),dummy.search(5))
+
+    def test_Mid_Tree_Result_Left_Deeper(self):
+        self.assertEqual(LCA.lowestCommonAncestor(dummy.root ,dummy.search(4), dummy.search(7)),dummy.search(2))
+
+
+
 if __name__ == '__main__':
     dummy = Tree()
 #
