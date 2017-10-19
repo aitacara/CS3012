14d56085deed8b85e3c3997ebb96887688c41d41
aitacara <aitacara@tcd.id>
2017-10-20 00:16:07 +0100

    Fixed tests and added solution code changed tree again

--- Sol.py
+++ Sol.py
@@ -1,5 +1,15 @@
 class LCA(object):
 
-	@staticmethod
-	def lowestCommonAncestor(root, p, q):
-	   	return root
+	
+	def lowestCommonAncestor(self, root, p, q):
+		if root in (None, p, q):
+			return root
+		left, right = [self.lowestCommonAncestor(child, p, q) \
+		for child in (root.left, root.right)]
+		# 1. If the current subtree contains both p and q,
+		#    return their LCA.
+		# 2. If only one of them is in that subtree,
+		#    return that one of them.
+		# 3. If neither of them is in that subtree,
+		#    return the node of that subtree.
+		return root if left and right else left or right
--- Test.py
+++ Test.py
@@ -5,39 +5,34 @@ from Sol import LCA
 class TestLCAMethod(unittest.TestCase):
     
     def test_3_Node_Tree(self):
-        self.assertEqual(LCA.lowestCommonAncestor(dummy.root ,dummy.search(5), dummy.search(1)),dummy.search(3))
+        self.assertEqual(caller.lowestCommonAncestor(dummy.root ,dummy.search(5), dummy.search(1)).key,dummy.search(3).key)
 
     def test_Root_As_Result(self):
-        self.assertEqual(LCA.lowestCommonAncestor(dummy.root ,dummy.search(6), dummy.search(8)),dummy.search(3))
+        self.assertEqual(caller.lowestCommonAncestor(dummy.root ,dummy.search(1), dummy.search(6)).key,dummy.search(3).key)
 
     def test_Both_Desendants_Same(self):
-        self.assertEqual(LCA.lowestCommonAncestor(dummy.root ,dummy.search(8), dummy.search(8)),dummy.search(1))
+        self.assertEqual(caller.lowestCommonAncestor(dummy.root ,dummy.search(8), dummy.search(8)).key,dummy.search(8).key)
 
     def test_Both_Desendants_Same_As_Root(self):
-        self.assertEqual(LCA.lowestCommonAncestor(dummy.root ,dummy.search(3), dummy.search(3)),dummy.search(3))
+        self.assertEqual(caller.lowestCommonAncestor(dummy.root ,dummy.search(3), dummy.search(3)).key,dummy.search(3).key)
 
     def test_Mid_Tree_Result_Right(self):
-        self.assertEqual(LCA.lowestCommonAncestor(dummy.root ,dummy.search(0), dummy.search(8)),dummy.search(1))
+        self.assertEqual(caller.lowestCommonAncestor(dummy.root ,dummy.search(4), dummy.search(6)).key,dummy.search(5).key)
 
     def test_Mid_Tree_Result_Left(self):
-        self.assertEqual(LCA.lowestCommonAncestor(dummy.root ,dummy.search(2), dummy.search(6)),dummy.search(5))
+        self.assertEqual(caller.lowestCommonAncestor(dummy.root ,dummy.search(2), dummy.search(0)).key,dummy.search(1).key)
 
     def test_Mid_Tree_Result_Left_Deeper(self):
-        self.assertEqual(LCA.lowestCommonAncestor(dummy.root ,dummy.search(4), dummy.search(7)),dummy.search(2))
+        self.assertEqual(caller.lowestCommonAncestor(dummy.root ,dummy.search(7), dummy.search(8)).key,dummy.search(8).key)
+
+    def test_Mid_Tree_Result_Left_split_level(self):
+        self.assertEqual(caller.lowestCommonAncestor(dummy.root ,dummy.search(7), dummy.search(4)).key,dummy.search(5).key)
 
 
 
 if __name__ == '__main__':
+    caller =LCA()
     dummy = Tree()
-#
-#         _______3______
-#       /              \
-#     ___5__          ___1__
-#   /      \        /      \
-#   6      _2       0       8
-#          /  \
-#          7   4
-    #treevals = [3,1,5,6,2,0,8,4,7]
     dummy.add_node(3)
     dummy.add_node(1)
     dummy.add_node(5)
@@ -47,7 +42,6 @@ if __name__ == '__main__':
     dummy.add_node(8)
     dummy.add_node(4)
     dummy.add_node(7)
-
-    print dummy.search(3).left.key
-    print dummy.search(3).right.key
+    dummy.traverse()
+   
     unittest.main()
