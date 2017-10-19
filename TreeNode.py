0dd313100e66f6c960a2de3232867076025a06e4
aitacara <aitacara@tcd.id>
2017-10-19 21:58:06 +0100

    Print tree method

--- TreeNode.py
+++ TreeNode.py
@@ -28,3 +28,16 @@ class Tree:
                 self._add(val, node.right)
             else:
                 node.right = TreeNode(val)
+
+    def printTree(root):
+      thislevel = [rootnode]
+      a = '                                 '
+      while thislevel:
+        nextlevel = list()
+        a = a[:len(a)/2]
+        for n in thislevel:
+          print a+str(n.value),
+          if n.left: nextlevel.append(n.left)
+          if n.right: nextlevel.append(n.right)
+          print
+          thislevel = nextlevel
